e2deddbddf6f33e101b078ad341bdf02686ab576
superspider2024 <diginalunlimited@gmail.com>
2024-12-22 14:20:33 -0800

    Finished up with the about page and root page and teh addding user functionality

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+const usernameInput = document.getElementById('username_input')
+const inputsend = document.getElementById('inputsend')
+
+
+inputsend.addEventListener('click',()=>{
+    const bro = usernameInput.value;
+
+    const lol = async (username1)=>{
+        const url = `http://localhost:3000/app/add/${username1}`;
+
+
+        try{
+        const response = await fetch(url,{
+            method:'GET'
+        })
+
+        if(!response){
+            throw new Error("Issue sending the request")
+        }
+
+        const data = await response.json()
+        alert(data)
+        
+
+        }catch(e){
+            alert(e.message)
+        }
+    }
+
+    lol(bro);
+    usernameInput.value='';
+
+
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {   
+      colors: {
+        'main-grey': '#454545', //main grey 
+        'light-grey': '#3C3C3C', //light grey
+        'light-lime':'#3CCF3C', //light lime 
+        'nice-grey':'##E2E6EC' //nice grey
+    },
+  },},
   plugins: [],
 }
 
