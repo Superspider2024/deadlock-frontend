6fd4bfd6521e55747e8c93bca69f8807c58710eb
superspider2024 <diginalunlimited@gmail.com>
2024-12-24 06:39:16 -0800

    pop

--- public/leader.js
+++ public/leader.js
@@ -16,12 +16,12 @@ const bro = async(player,pop)=>{
     imageContainer.appendChild(playerImage);
 
     const nameContainer = document.createElement('div');
-    nameContainer.classList.add('w-1/4', 'text-center mr-2');
+    nameContainer.classList.add('w-1/4', 'text-center', 'mr-2');
     nameContainer.textContent = player.insta;
 
 
     const votes = document.createElement('div');
-    votes.classList.add('w-1/4', 'text-center ml-4');
+    votes.classList.add('w-1/4', 'text-center', 'ml-4');
     votes.textContent = player.votes;
 
     const rankContainer = document.createElement('div');
